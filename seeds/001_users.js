bd95846919564f250aec15c814309c5cd6f512f4
Alicia Key <alicia.key@galvanize.com>
2018-04-24 13:25:02 -0600

    There is no admin.

--- seeds/001_users.js
+++ seeds/001_users.js
@@ -4,8 +4,7 @@ exports.seed = (knex, Promise) => (
       knex('users').insert([{
         id: 1,
         username: 'hello@me.com',
-        password: '$2a$10$REbk3qsthnNR8XGTeir1J.jdVOva3aURv3Um4MVqa6L8C2G3eHpg2',
-        is_admin: true
+        password: '$2a$10$REbk3qsthnNR8XGTeir1J.jdVOva3aURv3Um4MVqa6L8C2G3eHpg2'
       }])
     ))
     .then(() => knex.raw(`SELECT setval('users_id_seq', (SELECT MAX(id) FROM users));`))
