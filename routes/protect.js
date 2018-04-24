c9501f4a4151f3ef01047694c279903802ad15a3
Alicia Key <alicia.key@galvanize.com>
2018-04-24 14:46:41 -0600

    Grabs the username out of the auth header

--- routes/protect.js
+++ routes/protect.js
@@ -1,17 +1,29 @@
-// function protect(req, res, next) {
-//   console.log(req.headers)
-// }
-//
-// module.exports = protect
+const jwt = require('jsonwebtoken')
+require('dotenv').config()
 
 function protect(req, res, next) {
   const { authorization } = req.headers
 
-  if (authorization) {
-    next()
+  try {
+    if (!authorization) {
+      throw new Error('Authorization header not found')
+    }
+    const authParsed = authorization.split(' ')
+    if (authParsed.length !== 2) {
+      throw new Error('Authorization header is corrupt')
+    }
+    jwt.verify(authParsed[1], process.env.JWT_SECRET, (err, decoded) => {
+      if (err) {
+        throw new Error('JWT token is invalid')
+      }
+      else {
+        req.username = decoded.username
+        next()
+      }
+    })
   }
-  else {
-    res.status(403).send('Unauthorized')
+  catch (error) {
+    res.status(403).json({ error })
   }
 }
 
