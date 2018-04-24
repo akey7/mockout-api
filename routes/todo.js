41896073318efbe11dc8d4403f06ac8314496fe9
Alicia Key <alicia.key@galvanize.com>
2018-04-24 15:15:09 -0600

    List route works great.

--- routes/todo.js
+++ routes/todo.js
@@ -1,9 +1,14 @@
 const express = require('express')
 const router = express.Router()
+const knex = require('../knex')
 
 // By the time the request gets here, we can trust the username
 router.get('/', (req, res, next) => {
-  res.json({ todo: 'list' })
+  const { userId } = req
+  knex('todo')
+    .select('id', 'item')
+    .where('user_id', userId)
+    .then((result) => res.json(result))
 })
 
 module.exports = router
